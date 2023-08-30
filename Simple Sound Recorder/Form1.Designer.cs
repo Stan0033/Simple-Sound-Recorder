c4c6fa16e4da5113ddcbd4b2b4ba6db773103c5e
Stanislav Vladev <stan0033@abv.bg>
2023-08-31 02:46:33 +0300

    +

---
+++ Simple Sound Recorder/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Simple_Sound_Recorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label_recordingCurrentTime = new System.Windows.Forms.Label();
+            this.button_stoprecording = new System.Windows.Forms.Button();
+            this.button_record = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button_pause = new System.Windows.Forms.Button();
+            this.button_play = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(430, 55);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Audio recording device";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(412, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_recordingCurrentTime);
+            this.groupBox2.Controls.Add(this.button_stoprecording);
+            this.groupBox2.Controls.Add(this.button_record);
+            this.groupBox2.Location = new System.Drawing.Point(0, 61);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(430, 123);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Action";
+            // 
+            // label_recordingCurrentTime
+            // 
+            this.label_recordingCurrentTime.AutoSize = true;
+            this.label_recordingCurrentTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_recordingCurrentTime.ForeColor = System.Drawing.Color.Red;
+            this.label_recordingCurrentTime.Location = new System.Drawing.Point(182, 85);
+            this.label_recordingCurrentTime.Name = "label_recordingCurrentTime";
+            this.label_recordingCurrentTime.Size = new System.Drawing.Size(0, 25);
+            this.label_recordingCurrentTime.TabIndex = 2;
+            // 
+            // button_stoprecording
+            // 
+            this.button_stoprecording.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_stoprecording.Location = new System.Drawing.Point(12, 48);
+            this.button_stoprecording.Name = "button_stoprecording";
+            this.button_stoprecording.Size = new System.Drawing.Size(412, 23);
+            this.button_stoprecording.TabIndex = 1;
+            this.button_stoprecording.Text = "Finish current recording";
+            this.button_stoprecording.UseVisualStyleBackColor = true;
+            this.button_stoprecording.Click += new System.EventHandler(this.button_stoprecording_Click);
+            // 
+            // button_record
+            // 
+            this.button_record.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_record.Location = new System.Drawing.Point(12, 19);
+            this.button_record.Name = "button_record";
+            this.button_record.Size = new System.Drawing.Size(412, 23);
+            this.button_record.TabIndex = 0;
+            this.button_record.Text = "Start new recording";
+            this.button_record.UseVisualStyleBackColor = true;
+            this.button_record.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.trackBar1);
+            this.groupBox3.Controls.Add(this.button_pause);
+            this.groupBox3.Controls.Add(this.button_play);
+            this.groupBox3.Location = new System.Drawing.Point(0, 190);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(430, 162);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Recording";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = " ";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 79);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(412, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button_pause
+            // 
+            this.button_pause.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_pause.Location = new System.Drawing.Point(12, 48);
+            this.button_pause.Name = "button_pause";
+            this.button_pause.Size = new System.Drawing.Size(412, 23);
+            this.button_pause.TabIndex = 2;
+            this.button_pause.Text = "Pause";
+            this.button_pause.UseVisualStyleBackColor = true;
+            this.button_pause.Click += new System.EventHandler(this.button_pause_Click);
+            // 
+            // button_play
+            // 
+            this.button_play.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_play.Location = new System.Drawing.Point(12, 19);
+            this.button_play.Name = "button_play";
+            this.button_play.Size = new System.Drawing.Size(412, 23);
+            this.button_play.TabIndex = 1;
+            this.button_play.Text = "Play";
+            this.button_play.UseVisualStyleBackColor = true;
+            this.button_play.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.Location = new System.Drawing.Point(0, 358);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(430, 51);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Review";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(9, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(412, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Open Fodler";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 415);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Sound Recorder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button_stoprecording;
+        private System.Windows.Forms.Button button_record;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button_pause;
+        private System.Windows.Forms.Button button_play;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label_recordingCurrentTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Simple Sound Recorder/Form1.cs
@@ -0,0 +1,349 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Utils;
+using NAudio.Wave;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Simple_Sound_Recorder
+{
+    public partial class Form1 : Form
+    {
+        string currentRecordingDevice;
+        public Form1()
+        {
+            if (!Directory.Exists(Path.Combine(Environment.CurrentDirectory, "Recordings"))){ Directory.CreateDirectory(Path.Combine(Environment.CurrentDirectory, "Recordings")); }
+            InitializeComponent();
+            PopulateRecordingDevices();
+            InitializePlaybackTimer();
+            InitializeRecordingTimer();
+            currentEnumeration = GetNextAvailableRecordingNumber();
+
+
+        }
+        private static WaveInEvent waveIn;
+        private static WaveFileWriter waveWriter;
+        private static bool isRecording = false;
+        private static WaveOutEvent waveOut; // Added WaveOutEvent for playback
+        private static bool isPaused = false; // Added variable to track playback pause/resume
+        private Timer playbackTimer;
+        private Timer recordingTimer;
+        private TimeSpan lastPlaybackPosition = TimeSpan.Zero;
+        private bool userIsSeeking = false;
+        private AudioFileReader audioFile;
+        int currentEnumeration = 0;
+        private bool isPlaybackPaused = false;
+        private TimeSpan playbackPosition = TimeSpan.Zero; // Add this field
+        string mostRecentRecording;
+        string CurrentSoundFile;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private int GetNextAvailableRecordingNumber()
+        {
+            string[] wavFiles = Directory.GetFiles(Path.Combine(Environment.CurrentDirectory, "Recordings"), "recording*.wav")
+                .Select(Path.GetFileNameWithoutExtension)
+                .ToArray();
+
+            int maxEnumeration = 0;
+
+            foreach (string fileName in wavFiles)
+            {
+                if (int.TryParse(fileName.Substring("recording".Length), out int enumeration))
+                {
+                    maxEnumeration = Math.Max(maxEnumeration, enumeration);
+                }
+            }
+
+             
+            CurrentSoundFile = Path.Combine(Environment.CurrentDirectory, $"Recording{maxEnumeration+1}");
+           // MessageBox.Show(CurrentSoundFile);
+            return maxEnumeration + 1; ;
+        }
+        private void InitializeRecordingTimer()
+        {
+            recordingTimer = new Timer();
+            recordingTimer.Interval = 100; // Update every 100 milliseconds
+            recordingTimer.Tick += RecordingTimer_Tick;
+        }
+
+        private void InitializePlaybackTimer()
+        {
+            playbackTimer = new Timer();
+            playbackTimer.Interval = 100; // Update every 100 milliseconds
+            playbackTimer.Tick += PlaybackTimer_Tick;
+        }
+        private void RecordingTimer_Tick(object sender, EventArgs e)
+        {
+            if (isRecording)
+            {
+                TimeSpan currentTime = TimeSpan.FromSeconds((double)waveWriter.Length / waveWriter.WaveFormat.AverageBytesPerSecond);
+                label_recordingCurrentTime.Text = currentTime.ToString(@"m\:ss\:fff");
+            }
+        }
+        private void PopulateRecordingDevices()
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            MMDeviceCollection devices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+
+            foreach (MMDevice device in devices)
+            {
+                comboBox1.Items.Add(device.FriendlyName);
+            }
+
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0; // Set the selected item to the first device
+                currentRecordingDevice = comboBox1.Text;
+            }
+            else
+            {
+                MessageBox.Show( "No recording devices found. Closing app.");
+                Environment.Exit( 0 );
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+            
+                if (userIsSeeking && waveOut != null && waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    var audioFile = new AudioFileReader("output.wav");
+                    var trackbarPosition = (double)trackBar1.Value / trackBar1.Maximum;
+                    var newPosition = trackbarPosition * audioFile.TotalTime.TotalSeconds;
+                    audioFile.CurrentTime = TimeSpan.FromSeconds(newPosition);
+                }
+             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             StartRecording(comboBox1.SelectedIndex);
+        }
+        public void StartRecording(int selectedDeviceIndex)
+        {
+            if (selectedDeviceIndex < 0 || selectedDeviceIndex >= WaveInEvent.DeviceCount)
+            {
+                MessageBox.Show("Selected recording device is not available.");
+                return;
+            }
+
+            if (isRecording)
+            {
+                MessageBox.Show("Recording is already in progress.");
+                return;
+            }
+
+            int nextRecordingNumber = GetNextAvailableRecordingNumber();
+
+            string recordingsFolderPath = Path.Combine(Environment.CurrentDirectory, "Recordings");
+            Directory.CreateDirectory(recordingsFolderPath); // Ensure the directory exists
+
+            string outputPath = Path.Combine(recordingsFolderPath, $"recording{nextRecordingNumber}.wav");
+            CurrentSoundFile = outputPath;
+
+            waveIn = new WaveInEvent();
+            waveIn.DeviceNumber = selectedDeviceIndex;
+            waveIn.WaveFormat = new WaveFormat(44100, 1);
+
+            waveWriter = new WaveFileWriter(outputPath, waveIn.WaveFormat);
+
+            waveIn.DataAvailable += (sender, e) =>
+            {
+                if (waveWriter != null)
+                {
+                    waveWriter.Write(e.Buffer, 0, e.BytesRecorded);
+                }
+            };
+
+            waveIn.RecordingStopped += (sender, e) =>
+            {
+                if (waveWriter != null)
+                {
+                    waveWriter.Close();
+                    waveWriter.Dispose();
+                    waveWriter = null;
+                }
+            };
+
+            waveIn.StartRecording();
+            isRecording = true;
+            recordingTimer.Start();
+        }
+
+        public static void StopRecording()
+        {
+            if (isRecording)
+            {
+                waveIn.StopRecording();
+                waveWriter?.Dispose();
+                waveWriter = null;
+                isRecording = false;
+            }
+        }
+        public static void SaveRecording(string folderPath, string fileName)
+        {
+            if (isRecording)
+            {
+                MessageBox.Show("Stop recording before saving.");
+                return;
+            }
+
+            if (waveOut != null && waveOut.PlaybackState == PlaybackState.Playing)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+                System.Threading.Thread.Sleep(100); // Add a short delay
+            }
+
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                MessageBox.Show("Please provide a file name.");
+                return;
+            }
+
+            if (!fileName.EndsWith(".wav", StringComparison.OrdinalIgnoreCase))
+            {
+                fileName += ".wav"; // Ensure .wav extension
+            }
+
+            string fullPath = System.IO.Path.Combine(folderPath, fileName);
+
+            if (System.IO.File.Exists("output.wav"))
+            {
+                if (waveWriter != null)
+                {
+                    waveWriter.Close(); // Close the writer before copying
+                    waveWriter.Dispose();
+                    waveWriter = null;
+                }
+
+                System.IO.File.Copy("output.wav", fullPath, true);
+               // System.IO.File.Delete("output.wav");
+            }
+        }
+
+
+
+
+
+
+
+        private void StartPlayback()
+        {
+            if (waveOut != null)
+            {
+                if (isPlaybackPaused)
+                {
+                    // Resume playback from the last position if it was paused
+                    waveOut.Play();
+                    isPlaybackPaused = false;
+                    return;
+                }
+                else if (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    // Pause playback if it's playing
+                    waveOut.Pause();
+                    isPlaybackPaused = true;
+                    return;
+                }
+                else
+                {
+                    // Stop and dispose of the waveOut instance if it's not paused or playing
+                    waveOut.Stop();
+                    waveOut.Dispose();
+                    waveOut = null;
+                    playbackPosition = TimeSpan.Zero;
+                    playbackTimer.Stop();
+                }
+            }
+
+            string[] recordingFiles = Directory.GetFiles(Path.Combine(Environment.CurrentDirectory, "Recordings"), "recording*.wav");
+
+            if (recordingFiles.Length > 0)
+            {
+                string mostRecentRecording = recordingFiles.OrderBy(file => file).Last(); // Get the most recent recording
+                audioFile = new AudioFileReader(mostRecentRecording); // Open the audio file once
+                waveOut = new WaveOutEvent();
+                waveOut.Init(audioFile);
+
+                // Adjust the trackbar's maximum value to match the audio's length in milliseconds
+                trackBar1.Maximum = (int)audioFile.TotalTime.TotalMilliseconds;
+
+                playbackPosition = TimeSpan.Zero; // Reset the playback position
+                waveOut.Play();
+                playbackTimer.Start();
+            }
+            else
+            {
+                MessageBox.Show("No recorded audio to play.");
+            }
+        }
+        public int TurnTimeToMilliseconds(double time) { int n = (int)(time * 1000); return n; }
+        private void PlaybackTimer_Tick(object sender, EventArgs e)
+        {
+            if (waveOut != null && waveOut.PlaybackState == PlaybackState.Playing && audioFile != null)
+            {
+                playbackPosition = audioFile.CurrentTime;
+
+                // Update the trackbar value to match the current playback position
+                int trackbarValue = (int)(playbackPosition.TotalMilliseconds);
+                if (trackbarValue <= trackBar1.Maximum)
+                {
+                    trackBar1.Value = trackbarValue;
+                }
+            }
+        }
+
+        
+
+
+
+
+
+        private string FormatMilliseconds(int milliseconds)
+    {
+            TimeSpan timeSpan = TimeSpan.FromMilliseconds(milliseconds);
+            return timeSpan.ToString(@"m\:ss\:fff");
+        }
+
+
+        private void PausePlayback()
+        {
+            if (waveOut != null && waveOut.PlaybackState == PlaybackState.Playing)
+            {
+                waveOut.Pause();
+                isPlaybackPaused = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StartPlayback();
+        }
+
+        private void button_pause_Click(object sender, EventArgs e)
+        {
+            PausePlayback();
+        }
+
+        private void button_stoprecording_Click(object sender, EventArgs e)
+        {
+            StopRecording();
+            label_recordingCurrentTime.Text = string.Empty;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", Environment.CurrentDirectory);
+        }
+    }
+}
